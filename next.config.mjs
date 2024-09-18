5d130dec81d61a22637455b3197dd006cd6fdd26
DizzlyD <david4henery00@hotmail.com>
2024-09-18 19:26:36 +0100

    trying-new-config-again!

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
+  webpack(config, options) {
+    const { isServer } = options;
     config.module.rules.push(
       {
         test: /\.svg$/i,
@@ -9,11 +10,17 @@ const nextConfig = {
       },
       {
         test: /\.(ogg|mp3|wav|mpe?g)$/i,
+        exclude: config.exclude,
         use: [
           {
             loader: "url-loader",
             options: {
+              limit: config.inlineImageLimit,
+              fallback: "file-loader",
+              publicPath: `${config.assetPrefix}/_next/static/images/`,
+              outputPath: `${isServer ? "../" : ""}static/images/`,
               name: "[name]-[hash].[ext]",
+              esModule: config.esModule || false,
             },
           },
         ],
